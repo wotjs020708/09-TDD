ed3b57bd5bcc5a9c929f6615fd2dda528b04de25
JaeSunEo <wotjs020708@gmail.com>
2024-07-10 09:42:22 +0900

    hello_world

--- Hello_world.swift
+++ Hello_world.swift
@@ -1,1 +1,10 @@
 #!/usr/bin/env xcrun swift
+func main() {
+    guard CommandLine.argc > 1 else {
+        print("Hello, world!")
+        return
+    }
+    print("Hello, \(CommandLine.arguments[1])!")
+}
+
+main()
