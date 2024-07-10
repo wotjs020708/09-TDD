1b64c0432f3f001959b6d9946ef09c53f60ae388
JaeSunEo <wotjs020708@gmail.com>
2024-07-10 10:39:00 +0900

    Initialize Project HelloWolrd, add test TARGETS

---
+++ HelloWolrd/HelloWolrd/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloWolrd
+//
+//  Created by 어재선 on 7/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelloWolrd/HelloWolrd/HelloWolrdApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWolrdApp.swift
+//  HelloWolrd
+//
+//  Created by 어재선 on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloWolrdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
