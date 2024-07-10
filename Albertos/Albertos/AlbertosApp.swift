b59fec8593c01c672773b48f53bf752821f717a1
JaeSunEo <wotjs020708@gmail.com>
2024-07-10 13:10:28 +0900

    Initialize Project Albertos

---
+++ Albertos/Albertos/AlbertosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbertosApp.swift
+//  Albertos
+//
+//  Created by 어재선 on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct AlbertosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
