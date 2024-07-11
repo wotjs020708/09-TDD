1e38e60e357874bf70479e1bc5c83bdbcfd3fd6d
JaeSunEo <wotjs020708@gmail.com>
2024-07-11 11:35:46 +0900

    메뉴 리스트 viewModel 추가, MenuItem 의존 주입

--- Albertos/Albertos/MenuList.swift
+++ Albertos/Albertos/MenuList.swift
@@ -8,11 +8,11 @@
 import SwiftUI
 
 struct MenuList: View {
-    let sections: [MenuSection]
+    let viewModel: ViewModel
     
     var body: some View {
         List{
-            ForEach(sections) { section in
+            ForEach(viewModel.sections) { section in
                 Section(header: Text(section.category)) {
                     ForEach(section.items) { item in
                         MenuRow(viewModel: .init(item: item))
@@ -26,7 +26,7 @@ struct MenuList: View {
 
 #Preview {
     NavigationStack {
-        MenuList(sections: groupMenuByCategory(menu))
+        MenuList(viewModel: .init(menu: menu, menuGrouping: groupMenuByCategory))
         
     }
 }
